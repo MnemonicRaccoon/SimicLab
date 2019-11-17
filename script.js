c1df2b2f83f317aea87e0a3a132f465a9710c371
MnemonicRaccoon <57494359+MnemonicRaccoon@users.noreply.github.com>
2019-11-17 17:23:08 -0300

    bootstrap

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
 // Terrenos
 var plains = {
-	"img": "/SimicLab/display/plains.png",
-	"txt": "Planície da Alpha, primeira edição de Magic: The Gathering."
+    "title": "Planície",
+	"img": "/display/plains.png",
+	"alt": "Planície da Alpha, primeira edição de Magic: The Gathering."
 };
 
 var island = {
-	"img": "/SimicLab/display/island.png",
-	"txt": "Ilha da Alpha, primeira edição de Magic: The Gathering."
+    "title": "Ilha",
+	"img": "/display/island.png",
+	"alt": "Ilha da Alpha, primeira edição de Magic: The Gathering."
 };
 var swamp = {
-	"img": "/SimicLab/display/swamp.png",
-	"txt": "Pântano da Alpha, primeira edição de Magic: The Gathering."
+    "title": "Pântano",
+	"img": "/display/swamp.png",
+	"alt": "Pântano da Alpha, primeira edição de Magic: The Gathering."
 };
 var mountain = {
-	"img": "/SimicLab/display/mountain.png",
-	"txt": "Montanha da Alpha, primeira edição de Magic: The Gathering."
+    "title": "Montanha",
+	"img": "/display/mountain.png",
+	"alt": "Montanha da Alpha, primeira edição de Magic: The Gathering."
 };
 var forest = {
-	"img": "/SimicLab/display/forest.png",
-	"txt": "Floresta da Alpha, primeira edição de Magic: The Gathering."
+    "title": "Floresta",
+	"img": "/display/forest.png",
+	"alt": "Floresta da Alpha, primeira edição de Magic: The Gathering."
 };
 
-
-
-
-// Outras palavras
-var mana = {
-	"img": "none",
-	"txt": "No Multiverso de Magic, mana é a energia que serve de combustível para as mágicas conjuradas. Ela flui pelas linhas de força de cada plano, mantendo sua existência.<br>Em uma partida de Magic, o mana das cinco diferentes cores, além do mana incolor, são utilizados para conjurar magias e ativar habilidades. Em geral, magias e habilidades mais poderosas possuem um custo de mana mais elevado."
-};
-
-var virar = {
-	"img": "none",
-	"txt": "As cartas no campo de batalha podem estar em duas posições: viradas ou desviradas. "
-};
-
+// Display
 function display(x) {
+    if ($(window).width()<=768) {
+        $('#myModal').modal('show');
+    };
     document.getElementById("sidebar").display="block";
 	if (x.img === "none") {
 		document.getElementById("side-img").src = ("");
 		document.getElementById("side-txt").innerHTML = (x.txt);
 	} else	document.getElementById("side-img").src = (x.img);
-	document.getElementById("side-txt").innerHTML = (x.txt)
+	document.getElementById("side-img").alt = (x.alt);
+	document.getElementById("modal-title").innerHTML = (x.title);
+    document.getElementById("modal-img").src = (x.img);   
 };
+
+
+
+// Navbar 
+function hambMenu() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+
+// Hero
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
+
+// Popover
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover({html: true, placement: "top", trigger: "focus"});   
+});
